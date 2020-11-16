691765c986f94997fb8dd3008c73f26702ae0593
velxh <deribeiromello@outlook.com.br>
2020-11-16 19:30:48 -0300

    ProjetoDoAplicativo
    Primeiro commit do projeto

---
+++ AppWithSlider/app/src/main/java/com/example/appwithslider/MainFragment.java
@@ -0,0 +1,18 @@
+package com.example.appwithslider;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment{
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
+
