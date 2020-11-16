f3b9c7f4f4df1888e90b2fb206bc1b0a2f14c223
velxh <deribeiromello@outlook.com.br>
2020-11-16 20:09:39 -0300

    vazios

--- AppWithSlider/app/src/main/java/com/example/appwithslider/MainActivity.java
+++ AppWithSlider/app/src/main/java/com/example/appwithslider/MainActivity.java
@@ -11,49 +11,17 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.google.android.material.navigation.NavigationView;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.Message;
 import android.view.View;
-import android.widget.Button;
-import android.widget.SeekBar;
 import android.widget.Toast;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.UUID;
-
-
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-
-    MenuItem btnConexao;
-
-    BluetoothAdapter meuBluetoothAdapter = null;
-    BluetoothDevice meuDevice = null;
-    BluetoothSocket meuSocket = null;
-
-    private static final int SOLICITA_ATIVACAO = 1;
-    private static final int SOLICITA_CONEXAO = 2;
-    private  static final int MESSAGE_READ = 3;
-
-    boolean conexao = false;
-
-    private static String MAC = null;
-    UUID MEU_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
-
-    ConnectedThread connectedThread;
-    Handler mHandler;
-
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
     androidx.appcompat.widget.Toolbar toolbar;
@@ -66,19 +34,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btnConexao = (MenuItem) findViewById(R.id.btnConexao);
-
-        meuBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        if(meuBluetoothAdapter == null){
-            Toast.makeText(getApplicationContext(), "Seu dispositivo não possui a tecnologia bluetooth", Toast.LENGTH_LONG).show();
-        }
-        else if(!meuBluetoothAdapter.isEnabled()) {
-            Intent ativaBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(ativaBluetooth, SOLICITA_ATIVACAO);
-        }
-
 
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
 
@@ -102,13 +57,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         fragmentTransaction.commit();
     }
 
-
-
-
-
-
-
-
     //Implementando os botões no nav menu
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem){
@@ -120,23 +68,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             fragmentTransaction.commit();
         }
 
-        if(menuItem.getItemId() == R.id.btnConexao){
-            if(conexao){
-                //desconectar
-                try{
-                    meuSocket.close();
-                    conexao = false;
-                    Toast.makeText(getApplicationContext(), "O dispositivo foi desconectado", Toast.LENGTH_LONG).show();
-                }
-                catch(IOException erro){
-                    Toast.makeText(getApplicationContext(), "Erro: " + erro, Toast.LENGTH_LONG).show();
-                }
-            }
-            else{
-                //conectar
-                Intent abreLista = new Intent(MainActivity.this, ListaDispositivos.class);
-                startActivityForResult(abreLista, SOLICITA_CONEXAO);
-            }
+        if(menuItem.getItemId() == R.id.conexoes){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.content_fragment, new FragmentSecond());
+            fragmentTransaction.commit();
         }
 
         if(menuItem.getItemId() == R.id.control){
@@ -146,102 +82,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             fragmentTransaction.commit();
         }
         return true;
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case SOLICITA_ATIVACAO:
-                if (resultCode == Activity.RESULT_OK) {
-                    Toast.makeText(getApplicationContext(), "O bluetooth foi ativado com sucesso", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "O bluetooth não foi ativado", Toast.LENGTH_LONG).show();
-                    finish();
-                }
-                break;
-            case SOLICITA_CONEXAO:
-                if(resultCode == Activity.RESULT_OK){
-                    MAC = data.getExtras().getString(ListaDispositivos.ENDERECO_MAC);
-
-                    //Toast.makeText(getApplicationContext(), "MAC final: " + MAC, Toast.LENGTH_LONG).show();
-                    meuDevice = meuBluetoothAdapter.getRemoteDevice(MAC);
-
-                    try{
-                        meuSocket = meuDevice.createRfcommSocketToServiceRecord(MEU_UUID);
-
-                        meuSocket.connect();
-                        conexao = true;
-                        connectedThread = new ConnectedThread(meuSocket);
-                        connectedThread.start();
-                        Toast.makeText(getApplicationContext(), "Conectado com sucesso com: " + MAC, Toast.LENGTH_LONG).show();
-                    }
-                    catch(IOException erro){
-                        conexao = false;
-                        Toast.makeText(getApplicationContext(), "Ocorreu um erro: " + erro, Toast.LENGTH_LONG).show();
-                    }
-                }
-                else{
-                    Toast.makeText(getApplicationContext(), "Falha ao obter o MAC", Toast.LENGTH_LONG).show();
-                }
-        }
-    }
-    public class ConnectedThread extends Thread {
-        private final InputStream mmInStream;
-        private final OutputStream mmOutStream;
-
-        public ConnectedThread(BluetoothSocket socket) {
-            InputStream tmpIn = null;
-            OutputStream tmpOut = null;
-
-            // Get the input and output streams; using temp objects because
-            // member streams are final.
-            try{
-                tmpIn = socket.getInputStream();
-                tmpOut = socket.getOutputStream();
-            }
-            catch(IOException e){ }
-
-            mmInStream = tmpIn;
-            mmOutStream = tmpOut;
-        }
-
-        public void run() {
-            byte[] buffer = new byte[1024];
-            int bytes;
-
-            // Keep listening to the InputStream until an exception occurs.
-            while (true) {
-                try {
-                    // Read from the InputStream.
-                    bytes = mmInStream.read(buffer);
-
-                    String dadosBt = new String(buffer, 0, bytes);
-
-                    // Send the obtained bytes to the UI activity.
-                    mHandler.obtainMessage(MESSAGE_READ, bytes, -1, dadosBt).sendToTarget();
-
-                } catch (IOException e){
-                    break;
-                }
-            }
-        }
-
-        public final byte[] intToByteArray(int value) {
-            return new byte[] {
-                    (byte)(value >>> 24),
-                    (byte)(value >>> 16),
-                    (byte)(value >>> 8),
-                    (byte)value};
-        }
-
-        // Call this from the main activity to send data to the remote device.
-        public void enviar(String dadosEnviar) {
-            byte[] msgBuffer = dadosEnviar.getBytes();
-            try {
-                mmOutStream.write(msgBuffer);
-            } catch(IOException e) { }
-        }
     }
 }
 
